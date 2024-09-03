dcc7b943dff4e9124202c6257a8b42864caa233c
matheus <matheus@bolt360.com.br>
2024-09-03 12:47:27 -0300

    refactore

---
+++ objects/calculadora.js
@@ -0,0 +1,7 @@
+let calculadora = {
+    soma: function(a,b) {
+        console.log("A soma é " + (a + b))
+    }
+}
+
+calculadora.soma(4,3)
---
+++ objects/carro.js
@@ -0,0 +1,10 @@
+let carro = {
+    marca: "BMW",
+    modelo: "320i",
+    ano: 2024
+}
+
+carro.cor = "preto"
+carro.ano = 2025
+
+console.log(carro)
