05036e1abf267ed43b75136c868c0d470503f374
Matheus Freitas <mgsoutodelira@gmail.com>
2024-09-05 06:44:32 -0300

    refactore

---
+++ fundaments/math.js
@@ -0,0 +1,9 @@
+let raio = 3;
+const area = Math.PI * Math.pow(raio,2);
+
+console.log(Math.PI) //funcao Math apresenta varios metodos para realizar operacoes
+console.log(area.toFixed(2)) //toFixed limita a quantidade de casas decimais
+
+let number = 7.98267
+console.log(Math.floor(number)) //arredonda o valor para baixo
+console.log(Math.ceil(number)) //arredonda o valor para cima
---
+++ fundaments/string.js
@@ -0,0 +1,3 @@
+const nome = 'Matheus';
+const up = texto => texto.toUpperCase();
+console.log(`O nome ${nome} em UpperCase é ${up(nome)}`);
